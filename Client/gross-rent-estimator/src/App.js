edea590e667597ba7b6f5f8a95dae4777ba4852b
ParmbeerJohal <parmbeerjohal@gmail.com>
2021-03-09 21:57:22 -0800

    Added styling, created simple input

--- Client/gross-rent-estimator/src/App.js
+++ Client/gross-rent-estimator/src/App.js
@@ -1,24 +1,33 @@
-import logo from './assets/logo.svg';
+// import logo from './assets/logo.svg';
+import React from 'react';
 import './styles/App.css';
+import {
+  Container,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Row,
+  Col,
+} from 'reactstrap';
 
 function App() {
+
+  //TODO: create clickhandler to receive input
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="border border-primary inputContainer">
+        <Form>
+          <FormGroup className="centerForm">
+            <Label for="exampleText">Text Area</Label>
+            <Input type="text" name="text" id="exampleText" className="w-50"/>
+            <Button>Search</Button>
+        </FormGroup>
+        </Form>   
+      </Container>
+    </>
   );
 }
 
--- Client/gross-rent-estimator/src/index.js
+++ Client/gross-rent-estimator/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './tests/reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
